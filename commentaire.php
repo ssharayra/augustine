808cc8c4bd5f03473f38ca2b57bed48fdc60e82b
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2015-01-20 23:00:39 +0100

    comm

--- commentaire.php
+++ commentaire.php
@@ -1,11 +1,12 @@
-<?php 
-require_once('inc/bdd_conf.php');  
+<?php
+
+require_once('inc/bdd_conf.php');
 require_once('inc/fonctions.php');
 
-if (empty($_SESSION['authlevel']) || $_SESSION['authlevel'] < 2) {    
-    header('Location: index.php');
-    exit();
+if (empty($_SESSION['authlevel']) || $_SESSION['authlevel'] < 2) {
+  header('Location: index.php');
+  exit();
 }
 
-print_r($_SESSION); 
+print_r($_SESSION);
 ?>
