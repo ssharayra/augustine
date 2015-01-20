bb01a294a9a716abf6f01755e2db42971ac5cdfc
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2015-01-20 21:15:24 +0100

    initial commit

---
+++ user.php
@@ -0,0 +1,100 @@
+<?php 
+require_once('inc/bdd_conf.php');
+require_once('inc/fonctions.php');
+
+if (empty($_SESSION['authlevel']) || $_SESSION['authlevel'] < 3) {    
+    header('Location: index.php');
+    exit();
+}
+
+$users = array();
+
+?>
+
+<?php include_once('inc/header.php'); ?>
+
+<body style="background-color:black;">
+
+    <?php include_once('inc/menu.php'); ?>
+
+<?php 
+
+      // TODO : remettre de l'ordre ci-dessous en supprimant 
+      // les balises center et en utilisant des classes bootstrap twitter  
+
+?> 
+    <center>
+        <table style="width:100%;height:100%;">
+            <tr>
+                <td style="border:0px;width:100%;height:100%;">
+                    <center>
+                        <div style="width:500px;border:1px solid white;color:white;">
+                            <center>
+                                <p style="padding:3px;"><strong><big>Utilisateurs (<a href="index.php"><FONT
+                                                    COLOR="lightgreen">Retour</font></a>)</big></strong></p>
+                                <hr>
+                                <div style="padding:1em;height:auto;width:auto;"><u>G&eacute;rer un utilisateur</u><br>
+                                    <table style="width:100%;border:0px;">
+                                        <?php 
+                                        foreach ($users as $s) {
+                                            // Affichage des utilisateurs.
+                                            echo "<tr><form action='user.php?id=" . $s['id'] . "' method='post'>";
+                                            echo "<td><div style='border:1px solid white;color:white;background-color:black;width:100%;'><center><small>ID: " . $s['id'] . "</small></center></div></td>";
+                                            echo "<td><input type='text' name='username' style='border:1px solid white;color:white;background-color:black;text-align:center;width:100%;' value='" . $s['name'] . "'></td>";
+                                            echo "<td><input type='password' name='password' style='border:1px solid white;color:white;background-color:black;text-align:center;width:100%;' value='password'></td>";
+                                            echo "<td style='width:0px;'><input type='text' name='authlevel' style='width:25px;border:1px solid white;color:white;background-color:black;text-align:center;' value='" . $s['authlevel'] . "'></td>";
+                                            echo "<td><input type='submit' style='border:1px solid white;color:white;background-color:black;width:100%;' value='Modifier'></td>";
+                                            if ($s['authlevel'] == 3) {
+                                                echo "<td><div style='border:1px solid white;color:white;background-color:black;width:100%;'><center><small>--</small></center></div></td>";
+                                            } else {
+                                                echo "<td><div style='border:1px solid white;color:white;background-color:black;width:100%;cursor:pointer;' onclick='window.location = \"user.php?user&delete=" . $s['id'] . "\"'><center><small>&nbsp;&nbsp;Supprimer&nbsp;&nbsp;</small></center></div></td>";
+                                            }
+                                            echo "</form></tr>";                                            
+                                        }
+                                        ?>
+                                    </table>
+                                </div>
+                                <br><br>
+
+                                <div style="padding:1em;height:auto;width:auto;"><u>Cr&eacute;er un utilisateur</u><br>
+    <?php
+    $username = getPost('username', 'username');
+    $pw = getPost('password', 'password');
+    $authlevel = getPost('authlevel','1');
+    ?>
+                                    <form action="doUser.php" method="post">
+                                        <table style="width:100%;border:0px;">
+                                            <tr>
+                                                <td><input type='text' name='username'
+                                                           style='border:1px solid white;color:white;background-color:black;text-align:center;width:100%;'
+                                                           value='<?php echo $username; ?>'></td>
+                                                <td><input type='text' name='password'
+                                                           style='border:1px solid white;color:white;background-color:black;text-align:center;width:100%;'
+                                                           value='<?php echo $pw; ?>'></td>
+                                                <td style='width:0px;'><input type='text' name='authlevel'
+                                                                              style='width:25px;border:1px solid white;color:white;background-color:black;text-align:center;'
+                                                                              value='<?php echo $authlevel; ?>'></td>
+                                                <td><input type='submit'
+                                                           style='border:1px solid white;color:white;background-color:black;width:100%;'
+                                                           value='Cr&eacute;er'></td>
+                                            </tr>
+                                        </table>
+                                    </form>
+                                </div>
+                                </form>
+                                <small>
+                                    <p style="text-align:justify;padding:1em;"><u>Notice</u> : Vous ne pouvez modifier
+                                        qu'un utilisateur &agrave; la fois. Il s'agit d'une interface fragile. N'ex&eacute;cutez
+                                        aucune action sans en connaitre les cons&eacute;quences. Renseignez vous aupr&egrave;s
+                                        d'un administrateur.</p>
+                                    <u>Authlevel</u><br>1 => Mod&eacute;rateur<br>2 => Administrateur<br>3 =>
+                                    Super-Administrateur (1 seul normalement)<br>
+                                </small>
+                            </center>
+                        </div>
+                    </center>
+                </td>
+            </tr>
+        </table>
+    </center>
+    </body>
