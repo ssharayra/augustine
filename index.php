adc04b8768f8caf6422d814fe0ab1df9caee3285
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2015-01-20 21:07:50 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,80 @@
+<?php include_once('inc/bdd_conf.php');
+      include_once('inc/fonctions.php');
+?>
+
+<?php include_once('inc/header.php'); ?> 
+<body>
+
+<?php include_once('inc/menu.php'); ?>
+<p><img src="img/banniere.jpg"  alt=""></p>
+<br>
+<div class="container">
+    <center>
+    	<a href="http://www.vinci-melun.org"><img src="img/.jpg"></a>
+    	<a href="http://www."><img src="img/.jpg"></a>
+    	<a href="http://www.diderot.org"><img src="img/.jpg"></a>
+    </center>
+</div>
+<!-- Présentation de Augustine -->
+<div class="container">
+    <div >
+        <div class="page-header">
+            <h2>Augustine</h2>
+        </div>
+        <div class="row">
+            <p>
+                Ce projet est née par l'association de deux lyc&eacutees. Le <a
+                    href="http://www.vinci-melun.org">lyc&eacutee L&eacuteonard de Vinci</a><!--space-->
+                de Melun (BTS CPI et BAC Pro technicien d'usinage), qui se concentre sur la conception de la coque.
+                <!--space-->
+                Et le <a href="http://www.diderot.org">lycée Diderot</a> de Paris (BTS IRIS) qui se
+                concentre sur la partie &eacutel&eacutectronique de la voiture.
+            </p>
+            <br/>
+
+            <p>
+                Mais d'où vient le nom de ce projet "Augustine"?
+                <br/>
+                <br/>
+                Augustin... Augustine. Ce nom vient tout simplement de <a
+                    href="http://fr.wikipedia.org/wiki/Augustin_Mouchot" target="_blank">Augustin Mouchot</a>, l'homme
+                qui a invent&eacute le capteur solaire.
+            </p>
+     </div>   
+    </div>
+    <div>
+        <div class="page-header">
+            <h2>Challenge EducEco</h2>
+        </div>
+        <div class="row">
+            <p>
+                Le but de ce challenge est de consommer le moins d'&eacutenergie possible pour parcourir 17,14Km avec
+                une vitesse moyenne <!--space-->
+                minimum de 25Km/h.
+                <br/>
+                une demie-journ&eacutee est r&eacuteserv&eacutee &agrave la d&eacutecouverte de la piste et aux essais
+                libres. Les <!--space-->
+                contraintes du circuit urbain ne sont pas celles d'une piste de circuits automobile ainsi, les
+                v&eacutehicules seront appel&eacutes <!--space-->
+                par groupes homog&egravene class&eacutes selon:
+            </p>
+            <ul>
+                <li>Le groupe	 des v&eacutehicules &eacuteco-citadins (tous vecteurs d'&eacutenergie);</li>
+                <li>Le groupe des v&eacutehicules prototypes thermiques (tous carburants);</li>
+                <li>Le groupe des v&eacutehicules prototype &eacutelectriques.</li>
+            </ul>
+            <p> Pour un acc&egraves au site <a href="http://www.educeco.net/" target="_blank">Cliquez ici</a></p>
+        </div>
+    </div>
+    <div>
+        <div class="page-header">
+            <h2>Association Tournesol</h2>
+        </div>
+        <div class="row">
+        <p>
+	    </p>
+        </div>
+    </div>
+</div>
+
+<?php include_once('inc/footer.php');?>
